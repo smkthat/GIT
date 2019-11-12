b3ec79e8bb457b094f9795ce17862cd076a69e7c
smkthat <biggysmoke32@gmail.com>
2019-11-12 14:46:05 +0300

    added module 4

---
+++ SkillboxJava0toPro/module_4/6/DataApp2/src/Main.java
@@ -0,0 +1,49 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class Main {
+
+  private static final DateTimeFormatter RU_DATE_FORMATTER = DateTimeFormatter
+      .ofPattern("dd.MM.yyyy - EEE")
+      .localizedBy(new Locale("ru", "RU"));
+
+
+  public static void main(String[] args) {
+    System.out.println(
+        "\nВВЕДИТЕ ДАТУ РОЖДЕНИЯ\n"
+            + "Доступные форматы:\n"
+            + "\t- день.месяц.год\n");
+
+    LocalDate localBirthDate = null;
+    while (localBirthDate == null) {
+      System.out.print("\nВвод : ");
+      String inputDate = new Scanner(System.in).nextLine();
+      localBirthDate = validationInputDate(inputDate);
+    }
+
+    System.out.println("\nВывод:");
+    LocalDate todayDate = LocalDate.now();
+    int n = 0;
+    for (int i = localBirthDate.getYear(); i <= todayDate.getYear(); i++) {
+      LocalDate tmpDate = localBirthDate.plusYears(n);
+      if (tmpDate.isBefore(todayDate)) {
+        System.out.println(n + "\t-\t" + tmpDate.format(RU_DATE_FORMATTER));
+      }
+      n++;
+    }
+  }
+
+  private static LocalDate validationInputDate(String inputDate) {
+    LocalDate validInputDate = null;
+    try {
+      validInputDate = LocalDate.parse(inputDate, DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    } catch (DateTimeParseException e) {
+      System.err.println("\tДата введена неверно! Повторите ввод!");
+    }
+    return validInputDate;
+  }
+}
