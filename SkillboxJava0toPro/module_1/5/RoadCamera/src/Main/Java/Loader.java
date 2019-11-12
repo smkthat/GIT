ca2a11217f78c6161d5b31f055ebb2b5b884ff80
smkthat <biggysmoke32@gmail.com>
2019-11-12 15:00:58 +0300

    refactor and rename Main class to Loader

--- SkillboxJava0toPro/module_1/5/RoadCamera/src/Main/Java/Loader.java
+++ SkillboxJava0toPro/module_1/5/RoadCamera/src/Main/Java/Loader.java
@@ -1,8 +1,10 @@
+package Main.Java;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class RoadCamera
+public class Loader
 {
     public static void main(String[] args) throws IOException
     {
