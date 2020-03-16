86481ece16bba93099d333a722681b42b2c70bf8
smkthat <biggysmoke32@gmail.com>
2020-03-16 12:12:07 +1100

    Revert "added AffineTransform resizing"
    This reverts commit f1b69278531bb164a85735a30ffa81170048433d.
    
    # Conflicts:
    #	SkillboxJava0toPro/module_9/JSONParser_MSKMetro/src/metro/Connection.java
    #	SkillboxJava0toPro/module_9/MSKMetroWikiParser/ff
    #	SkillboxJava0toPro/module_9/MSKMetroWikiParser/map.json
    #	SkillboxJava0toPro/module_9/MSKMetroWikiParser/src/Connection.java
    #	SkillboxJava0toPro/module_9/MSKMetroWikiParser/src/HTMLParser.java
    #	SkillboxJava0toPro/module_9/MSKMetroWikiParser/src/JSOUPParser.java
    #	SkillboxJava0toPro/module_9/MSKMetroWikiParser/src/Line.java
    #	SkillboxJava0toPro/module_9/MSKMetroWikiParser/src/Parser.java
    #	SkillboxJava0toPro/module_9/MSKMetroWikiParser/src/Station.java
    #	SkillboxJava0toPro/module_9/MSKMetroWikiParser/src/TableGenerator.java
    #	SkillboxJava0toPro/module_9/MSKMetroWikiParser/src/metro/Connection.java
    #	SkillboxJava0toPro/module_9/MSKMetroWikiParser/src/metro/Line.java
    #	SkillboxJava0toPro/module_9/MSKMetroWikiParser/src/metro/Station.java

--- SkillboxJava0toPro/module_9/MSKMetroWikiParser/src/Main.java
+++ SkillboxJava0toPro/module_9/MSKMetroWikiParser/src/Main.java
@@ -18,7 +18,7 @@ public class Main {
           + "_%D0%BC%D0%B5%D1%82%D1%80%D0%BE%D0%BF%D0%BE%D0%BB%D0%B8%D1%82%D0%B5%D0%BD%D0%B0";
 
   public static void main(String[] args) {
-    JSOUPParser jsoupParser = new JSOUPParser(URL_STR);
+    HTMLParser htmlParser = new HTMLParser(URL_STR);
 
   }
 }
---
+++ SkillboxJava0toPro/module_9/MSKMetroWikiParser/src/Station.java
@@ -0,0 +1,17 @@
+public class Station {
+  private Line line;
+  private String name;
+
+  public Station(String name, Line line) {
+    this.name = name;
+    this.line = line;
+  }
+
+  public Line getLine() {
+    return line;
+  }
+
+  public String getName() {
+    return name;
+  }
+}
